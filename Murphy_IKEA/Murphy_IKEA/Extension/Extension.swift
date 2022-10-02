3405c0d3b5a4a21feb50dcc3466cc045e899a70c
Somin Park <ramgoods2@gmail.com>
2022-10-02 17:49:09 +0900

    [Add] 프로젝트 셋팅

---
+++ Murphy_IKEA/Murphy_IKEA/Extension/Extension.swift
@@ -0,0 +1,8 @@
+//
+//  Extension.swift
+//  Murphy_IKEA
+//
+//  Created by Somin Park on 2022/10/02.
+//
+
+import Foundation
---
+++ Murphy_IKEA/Murphy_IKEA/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  Murphy_IKEA
+//
+//  Created by Somin Park on 2022/10/02.
+//
+
+import Foundation
