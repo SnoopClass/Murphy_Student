c71b483499784938f28731261bbdf6c4d24069b7
Somin Park <ramgoods2@gmail.com>
2022-10-02 21:18:58 +0900

    [Feat] 검색탭 타이틀 label 추가 (#3)

--- Murphy_IKEA/Murphy_IKEA/View/SearchViewController.swift
+++ Murphy_IKEA/Murphy_IKEA/View/SearchViewController.swift
@@ -14,16 +14,32 @@ class SearchViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
         configureScrollView()
+        configureTitle()
     }
     
     func configureScrollView() {
         self.view.addSubview(scrollView)
         scrollView.backgroundColor = .yellow
         scrollView.translatesAutoresizingMaskIntoConstraints = false
-        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        NSLayoutConstraint.activate([
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
-
+    
+    func configureTitle() {
+        let titleLabel = UILabel()
+        titleLabel.text = "검색"
+        titleLabel.font = UIFont.systemFont(ofSize: 25, weight: .bold)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(titleLabel)
+        
+        let margins = view.layoutMarginsGuide
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: margins.topAnchor, constant: 25),
+            titleLabel.leadingAnchor.constraint(equalTo: margins.leadingAnchor, constant: 10)
+        ])
+    }
 }
