d69260d097ebba5a1040520e009a524fba8964c1
Somin Park <ramgoods2@gmail.com>
2022-10-04 16:40:28 +0900

    [Feat] 스크롤뷰 전반적인 UI layout 설정 (#3)

--- Murphy_IKEA/Murphy_IKEA/View/SearchViewController.swift
+++ Murphy_IKEA/Murphy_IKEA/View/SearchViewController.swift
@@ -7,79 +7,63 @@
 
 import UIKit
 
-public class SearchBarButton: UIView {
-    let imageView: UIImageView = {
-        let view = UIImageView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.image = UIImage(systemName: "magnifyingglass")
-        view.tintColor = .gray
-        return view
-    }()
-    
-    let descriptionLabal: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "무엇을 찾고 있나요?"
-        label.textColor = .gray
-        label.font = UIFont.systemFont(ofSize: 12, weight: .regular)
-        return label
-    }()
-    
-    override public init(frame: CGRect) {
-        super.init(frame: frame)
-        setupLayout()
-    }
-    
-    required convenience init?(coder: NSCoder) {
-        self.init(coder: coder)
-        setupLayout()
-    }
-    
-    func setupLayout() {
-        addSubview(imageView)
-        addSubview(descriptionLabal)
-        layer.cornerRadius = 20
-        layer.borderColor = UIColor.black.cgColor
-        layer.borderWidth = 1
-        
-        NSLayoutConstraint.activate([
-            imageView.widthAnchor.constraint(equalToConstant: 15),
-            imageView.heightAnchor.constraint(equalToConstant: 15),
-            imageView.centerYAnchor.constraint(equalTo: centerYAnchor),
-            imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
-            descriptionLabal.centerYAnchor.constraint(equalTo: centerYAnchor),
-            descriptionLabal.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 10)
-        ])
-    }
-}
-
 class SearchViewController: UIViewController {
 
     let scrollView = UIScrollView()
+    let contentView = UIView()
+    private var safeArea: UILayoutGuide {
+        get { self.view.safeAreaLayoutGuide }
+    }
     let titleLabel = UILabel()
     let searchBarButtonView: SearchBarButton = {
         let view = SearchBarButton()
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
+    let recentView = UIView()
+    let searchProductView = UIView()
+    let campaignView = UIView()
+    let popularView = UIView()
+    let infoView = UIView()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemBackground
+        configureView()
+        
+    }
+    
+    func configureView() {
         configureScrollView()
         configureTitle()
         configureSearchBarButton()
+        configureRecentView()
+        configureSearchProductView()
+        configureCampaignView()
+        configurePopularView()
+        configureInfoView()
     }
     
     func configureScrollView() {
-        self.view.addSubview(scrollView)
-        scrollView.backgroundColor = .yellow
+        view.addSubview(scrollView)
         scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.alwaysBounceVertical = true
         NSLayoutConstraint.activate([
-            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
-            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            scrollView.frameLayoutGuide.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            scrollView.frameLayoutGuide.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            scrollView.frameLayoutGuide.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            scrollView.frameLayoutGuide.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+        ])
+        scrollView.addSubview(contentView)
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
+            contentView.heightAnchor.constraint(greaterThanOrEqualTo: view.heightAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor)
         ])
     }
     
@@ -87,22 +71,87 @@ class SearchViewController: UIViewController {
         titleLabel.text = "검색"
         titleLabel.font = UIFont.systemFont(ofSize: 25, weight: .bold)
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        scrollView.addSubview(titleLabel)
-        let margins = view.layoutMarginsGuide
+        contentView.addSubview(titleLabel)
         NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: margins.topAnchor, constant: 25),
-            titleLabel.leadingAnchor.constraint(equalTo: margins.leadingAnchor, constant: 10)
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 25),
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10)
         ])
     }
     
     func configureSearchBarButton() {
-        scrollView.addSubview(searchBarButtonView)
+        contentView.addSubview(searchBarButtonView)
         NSLayoutConstraint.activate([
             searchBarButtonView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
-            searchBarButtonView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 10),
-            searchBarButtonView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: 10),
+            searchBarButtonView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            searchBarButtonView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 10),
             searchBarButtonView.heightAnchor.constraint(equalToConstant: 40),
             searchBarButtonView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor)
+        ])
+    }
+    
+    func configureRecentView() {
+        contentView.addSubview(recentView)
+        recentView.translatesAutoresizingMaskIntoConstraints = false
+        recentView.backgroundColor = .purple
+        NSLayoutConstraint.activate([
+            recentView.topAnchor.constraint(equalTo: searchBarButtonView.bottomAnchor, constant: 40),
+            recentView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            recentView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            recentView.heightAnchor.constraint(equalToConstant: 250),
+            recentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor)
+        ])
+    }
+    
+    func configureSearchProductView() {
+        contentView.addSubview(searchProductView)
+        searchProductView.translatesAutoresizingMaskIntoConstraints = false
+        searchProductView.backgroundColor = .green
+        NSLayoutConstraint.activate([
+            searchProductView.topAnchor.constraint(equalTo: recentView.bottomAnchor, constant: 40),
+            searchProductView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            searchProductView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            searchProductView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            searchProductView.heightAnchor.constraint(equalToConstant: 780)
+        ])
+    }
+    
+    func configureCampaignView() {
+        contentView.addSubview(campaignView)
+        campaignView.translatesAutoresizingMaskIntoConstraints = false
+        campaignView.backgroundColor = .orange
+        NSLayoutConstraint.activate([
+            campaignView.topAnchor.constraint(equalTo: searchProductView.bottomAnchor, constant: 50),
+            campaignView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            campaignView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            campaignView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            campaignView.heightAnchor.constraint(equalToConstant: 570)
+        ])
+    }
+    
+    func configurePopularView() {
+        contentView.addSubview(popularView)
+        popularView.translatesAutoresizingMaskIntoConstraints = false
+        popularView.backgroundColor = .blue
+        NSLayoutConstraint.activate([
+            popularView.topAnchor.constraint(equalTo: campaignView.bottomAnchor, constant: 60),
+            popularView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            popularView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            popularView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            popularView.heightAnchor.constraint(equalToConstant: 830)
+        ])
+    }
+    
+    func configureInfoView() {
+        contentView.addSubview(infoView)
+        infoView.translatesAutoresizingMaskIntoConstraints = false
+        infoView.backgroundColor = .systemPink
+        NSLayoutConstraint.activate([
+            infoView.topAnchor.constraint(equalTo: popularView.bottomAnchor, constant: 50),
+            infoView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            infoView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            infoView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            infoView.heightAnchor.constraint(equalToConstant: 730),
+            infoView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
         ])
     }
 }
